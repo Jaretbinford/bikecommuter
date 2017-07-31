f7b5b8819b42eb0722211cb90e8b4630296a74f1
Jaret Binford <jaretbinford@gmail.com>
2017-07-31 01:02:14 -0400

    Added config script

--- src/bikecommuter/core.clj
+++ src/bikecommuter/core.clj
@@ -1,6 +1,24 @@
-(ns bikecommuter.core)
+(ns bikecommuter.core
+  (:require [com.stuartsierra.component :as c]
+            [arachne.log :as log]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Widget []
+  c/Lifecycle
+   (start [this]
+     (log/info :msg "Hello, World!")
+     this)
+   (stop [this]
+     (log/info :msg "Goodnight!")
+     this))
+
+
+;example run:
+;(def w (->Widget))
+;(c/start w)
+;end example
+
+
+(defn make-widget
+  "Constructor for a Widget"
+  []
+  (->Widget))
